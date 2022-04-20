a4c13b48c42abb8a3594eb8ac2d8f375f0c80990
es-ShadiSaeed2020@alexu.edu.eg <es-ShadiSaeed2020@alexu.edu.eg>
2022-04-20 09:39:45 +0200

    Added User Profile model, manager and migrations

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ Profiles Rest Api Code.
 
 ## helping lines
 
-to access virtual server: vagrant ssh
-to activate venv: source ~/env/bin/activate
+to access virtual server: vagrant ssh \n
+to activate venv: source ~/env/bin/activate \n
+to run server at port 8000: python manage.py runserver 0.0.0.0:8000 \n
+to make migrations : python manage.py makemigrations {app name} \n
+to migrate : python manage.py migrate
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Manager for user profile"""
+    
+    def create_user(self,email,name,password=None):
+        """Create new user"""
+        if not email:
+            raise ValueError('Users must have an Email') #checking if passed email has value not null nor empty
+        
+        email = self.normalize_email(email) # normalize_email normalizes second half after the @ in the email
+        user = self.model(email=email, name=name) # Creating the user using model
+        
+        user.set_password(password) # Setting password Hashed
+        user.save(using=self._db) # standard django way to save, to enable multiple databases of needed
+        
+        return user
+    
+    def create_superuser(self,email,name,password):
+        """Create new superuser with given details"""
+        
+        user = self.create_user(email,name,password) # creating standard user first
+        
+        user.is_superuser = True # Came from PermissionsMixin to our UserProfile model
+        user.is_staff = True
+        user.save(using=self._db)
+        
+        return user    
+
+
+class UserProfile (AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserProfileManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+    
+    def get_full_name(self):
+        """Retrive full name of user"""
+        return self.name
+    
+    def get_short_name(self):
+        """Retrive short name of user"""
+        return self.name
+    
+    def __str__(self):
+        """Retutn string represntation for our username
+        """
+        return self.email
