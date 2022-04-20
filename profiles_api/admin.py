f730daee8540a80fb1a33b334a86381a5f12f243
es-ShadiSaeed2020@alexu.edu.eg <es-ShadiSaeed2020@alexu.edu.eg>
2022-04-20 10:08:52 +0200

    Enable Django admin for user prfile

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from profiles_api.models import UserProfile
+
+admin.site.register(UserProfile) # registering UserProfile model to the admin site to access it
 
 # Register your models here.
